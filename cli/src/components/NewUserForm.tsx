306cf0e1537f0dc7575d74ff01c2c7f94b111c94
Silas Matos <silas.progamacao@gmail.com>
2024-03-23 15:13:08 -0300

    add

---
+++ cli/src/components/NewUserForm.tsx
@@ -0,0 +1,36 @@
+import { gql, useMutation, useQuery } from '@apollo/client'
+import { FormEvent, useState } from 'react'
+import { GET_USER } from '../App'
+
+const CREATE_USER = gql`
+  mutation createNewUser($name: String!) {
+    createUser(name: $name) {
+      id
+      name
+    }
+  }
+`
+
+export default function NewUserForm() {
+  const [name, setName] = useState('')
+  const [createUser, { data, loading, error }] = useMutation(CREATE_USER)
+  async function handleCreateUser(event: FormEvent) {
+    event.preventDefault()
+
+    if (!name) {
+      return
+    }
+    await createUser({
+      variables: {
+        name
+      },
+      refetchQueries: [GET_USER]
+    })
+  }
+  return (
+    <form onSubmit={handleCreateUser}>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <button type="submit">Enviar</button>
+    </form>
+  )
+}
